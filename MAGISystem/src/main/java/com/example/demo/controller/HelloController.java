4690148d21205767c96fb66efc6827c31c989781
yokouchiRyouta <ryoutako0901@gmail.com>
2018-09-04 16:31:11 +0900

    I have added some code in HelloController

--- MAGISystem/src/main/java/com/example/demo/controller/HelloController.java
+++ MAGISystem/src/main/java/com/example/demo/controller/HelloController.java
@@ -10,6 +10,10 @@ public class HelloController {
 	@RequestMapping("/")
 	public String firstView(Model model) {
 		model.addAttribute("hello", "ごはんが美味しい季節です");
+		model.addAttribute("gmsg1", "グローバルメッセージ1");
+		model.addAttribute("gmsg2", "グローバルメッセージ2");
+		model.addAttribute("gmsg3", "グローバルメッセージ3");
+
 		return "layout";
 	}
 }
